ca0dc323f240c6be6672f6dc8cb31cbbc5b97da0
Ahmed-A-Alazazi <hamadaaziz28@gmail.com>
2026-07-25 21:35:43 +0300

    third lecture modifications

--- StudentsManagmentSystem/main.dart
+++ StudentsManagmentSystem/main.dart
@@ -1,7 +1,79 @@
 import 'dart:io';
 
-List<Map<String,dynamic>> students = [];
-int counter = 1;
+List<Person> listOfPersons = [];
+
+abstract class Person{
+
+  static int _counter = 1;
+
+  final int id;
+  late String name;
+  late String department;
+
+
+  Person({required this.id, required this.name, required this.department});
+
+  void printDetails();
+
+  static int generateID() {
+   return _counter++; 
+  }
+
+  bool hasID (int PersonID) {
+    return this.id == PersonID;
+  }
+}
+
+class Student extends Person {
+
+  late int level;
+  late double _gpa; 
+
+  double get gpa => _gpa;
+
+  set setGPA (double studentGPA) {
+    if (studentGPA >= 0 && studentGPA <= 100) {
+      _gpa = studentGPA;
+    }
+    else {
+      throw Exception("GPA must be between 0 and 100");
+    }
+  }
+
+  Student({
+    required super.id,
+    required super.name,
+    required super.department,
+    required this.level,
+    required double studentGPA
+  }) {
+    this.setGPA = studentGPA;
+  }
+
+  Student.Guest() : this.level = 1, this._gpa = 0, super(id : 0, name : 'Unknown', department : 'Unknown');
+
+  @override
+  void printDetails() {
+    print("Student's ID: ${super.id} \nName: ${super.name} /nDepartment: ${super.department} \nLevel: ${this.level} \nGPA: ${this._gpa}");
+  }
+}
+
+class Doctors extends Person {
+
+  late int salary;
+
+  Doctors({
+    required super.id,
+    required super.name,
+    required super.department,
+    required this.salary
+  });
+
+  @override
+  void printDetails() {
+    print("Doctor's ID: ${super.id} \nName: ${super.name} /nDepartment: ${super.department} \nSalary: ${this.salary}");
+  }
+}
 
 void showMessage() {
   
@@ -9,35 +81,72 @@ void showMessage() {
   print("2- Remove Student");
   print("3- Show Students");
   print("4- Find Student");
-  print("5- Exit");
-  stdout.write("Choose Number: ");
+  print("5- Add Guest Student");
+  print("6- Add Doctor Student");
+  print("7- Exit");
+  stdout.write("\nChoose Number: ");
 }
 
 void addStudent () {
   stdout.write("Enter Student's Name: ");
-  String name = stdin.readLineSync() ??"";
+  String FullName = stdin.readLineSync() ??"";
   
   stdout.write("Enter Student's Level: ");
-  int level = int.tryParse(stdin.readLineSync() ?? "") ?? 1;
+  int studentLevel = int.tryParse(stdin.readLineSync() ?? "") ?? 1;
 
   stdout.write("Enter Student's Department: ");
-  String department = stdin.readLineSync() ?? "";
+  String studentDepartment = stdin.readLineSync() ?? "";
 
   stdout.write("Enter Student's GPA: ");
-  double gpa = double.tryParse(stdin.readLineSync() ?? "") ?? 0.0;
+  double studentGPA = double.tryParse(stdin.readLineSync() ?? "") ?? 0.0;
 
-  students.add({
-    'id': counter++,
-    'name': name,
-    'department': department,
-    'level': level,
-    'gpa': gpa
-  });
+  try {
+  listOfPersons.add(
+    Student(
+    id: Person.generateID(),
+    name: FullName,
+    level: studentLevel,
+    department: studentDepartment,
+    studentGPA: studentGPA
+  )
+  );
 
-  print("Student Was Added Successfully!.");
+  print("Student Has Added Successfully!.");
+  } 
+  catch (error) {
+    print(error);
+  }
 }
 
-void removeStudent() {
+void addDoctor() {
+
+  stdout.write("Enter Doctor's Name: ");
+  String doctorFullName = stdin.readLineSync() ??"";
+
+  stdout.write("Enter Doctor's Department: ");
+  String doctorDepartment = stdin.readLineSync() ?? "";
+
+  stdout.write("Enter Doctor's Salary: ");
+  int doctorSalary = int.tryParse(stdin.readLineSync() ?? "") ?? 1;
+
+  listOfPersons.add(
+    Doctors(
+    id: Person.generateID(),
+    name: doctorFullName,
+    department: doctorDepartment,
+    salary: doctorSalary
+  )
+  );
+
+  print("Doctor Has Added Successfully");
+}
+
+void addGuestStudent(){
+  listOfPersons.add(Student.Guest());
+  print('Guest Student Added Successfuly!');
+}
+
+void removePerson() {
   stdout.write("Enter Student's ID: ");
   int? id = int.tryParse(stdin.readLineSync() ?? "");
 
@@ -46,9 +155,9 @@ void removeStudent() {
     return;
   } 
 
-  if (students.any((student) => student['id'] == id)) {
-      students.removeWhere((student) => student['id'] == id);
-      print("Student Was Removed Successfully!.");
+  if (listOfPersons.any((Person) => Person.hasID(id))) {
+      listOfPersons.removeWhere((Person) => Person.hasID(id));
+      print("Student Has Removed Successfully!.");
   } 
   else {
       print("Student Was Not Found!.");
@@ -56,18 +165,18 @@ void removeStudent() {
 
 }
 
-void showStudent() {
-  if(students.isEmpty) {
+void showPerson() {
+  if(listOfPersons.isEmpty) {
     print("Student's List is Empty!.");
     return;
   }
 
-  for (var student in students) {
-    print("ID: ${student['id']} \nName: ${student['name']}. \nDepartment: ${student['department']}. \nLevel: ${student['level']}. \nGPA: ${student['gpa']}.");
+  for (Person student in listOfPersons) {
+    student.printDetails();
   }
 }
 
-void findStudent() {
+void findPerson() {
   stdout.write("Enter Student's ID: ");
   int? id = int.tryParse(stdin.readLineSync() ?? "");
 
@@ -76,9 +185,9 @@ void findStudent() {
     return;
   } 
 
-  for (var student in students) {
-    if (student['id'] == id) {
-      print("ID: ${student['id']} \nName: ${student['name']}. \nDepartment: ${student['department']}. \nLevel: ${student['level']}. \nGPA: ${student['gpa']}.");
+  for (Person student in listOfPersons) {
+    if (student.hasID(id)) {
+      student.printDetails();
       return;
     }
   }
@@ -102,17 +211,22 @@ void main() {
         addStudent();
         break;
       case "2":
-        removeStudent();
+        removePerson();
         break;
       case "3":
-        showStudent();
+        showPerson();
         break;
       case "4":
-        findStudent();
+        findPerson();
         break;
       case "5":
-        work = false;
+        addGuestStudent();
         break;
+      case "6":
+        addDoctor();
+        break;
+      case "7":
+        work = false;
       default:
         print("Invalid Input!.");
     }
